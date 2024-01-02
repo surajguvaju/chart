e6d1266055347db9994f2118ee20f74a8758a1ae
surajguvaju <99315726+surajguvaju@users.noreply.github.com>
2024-01-02 20:57:14 +0545

    Add files via upload

---
+++ Home.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div>
+      <div className="flex flex-row bg-slate-400 border border-solid min-w-[1400px] ">
+        <div className="bg-green-300 w-1/4 h-full"> Nav</div>
+        <div className="bg-yellow-200 w-2/4 h-full">Scroll</div>
+        <div className="bg-blue-400 w-1/4 h-full">Graph</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
